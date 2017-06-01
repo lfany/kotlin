b0728af49aa742c7aa8b3750f30db44f37ecb62d
hello <hello@ifnot.cc>
2017-06-01 10:21:34 +0800

    update fun

--- hello/main/kotlin/hello.kt
+++ hello/main/kotlin/hello.kt
@@ -4,4 +4,12 @@
 
 fun main(args: Array<String>) {
     println("Hello!")
+    printSum(1, 2)
 }
+
+// Unit 返回类型可以省略：
+// 字符串模板
+fun printSum(a: Int, b: Int): Unit {
+    println("$a + $b = ${a + b}")
+}
+

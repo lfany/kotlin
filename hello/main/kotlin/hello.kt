be55aa064bb90495a88bff9b58e3d0a47075f64c
hello <hello@ifnot.cc>
2017-06-01 10:37:23 +0800

    update

--- hello/main/kotlin/hello.kt
+++ hello/main/kotlin/hello.kt
@@ -6,7 +6,9 @@ fun main(args: Array<String>) {
     println("Hello!")
     printSum(1, 2)
 
-    println(Data())
+    val data = Data()
+    data.email = "xx"
+    println(data)
 }
 
 // Unit 返回类型可以省略：
@@ -18,5 +20,5 @@ fun printSum(a: Int, b: Int): Unit {
 
 // https://www.kotlincn.net/docs/reference/idioms.html
 // 创建 DTOs（POJOs/POCOs） 函数的默认参数
-data class Data(val name: String = "xxx", val email: String = "xxx@xxx.com")
+data class Data(val name: String = "xxx", var email: String = "xxx@xxx.com")
 
